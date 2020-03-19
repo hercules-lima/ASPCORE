0da7bf97ac58a3def37e75b2f926d8b6f24a2459
hercules <herculeslcdev@gmail.com>
2020-03-19 13:37:15 -0300

    Controllers

---
+++ ASPCORE/Controllers/AdminController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPCORE.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("Olá Mundo  - School of Net");
+        }
+
+        public IActionResult schoolOfNet()
+        {
+            return Content("Aprendendo ASP.NET Core na School of net!");
+        }
+    }
+}
